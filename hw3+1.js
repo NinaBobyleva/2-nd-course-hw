7bb9c856ca6d7c2359f4531b3e062deeb482ab70
Nina Bobyleva <2847171@mail.ru>
2023-12-23 17:57:38 +0300

    Changed the files style.css and mobile.css

---
+++ hw3+1.js
@@ -0,0 +1,8 @@
+let password = 'Пароль';
+let answer = prompt('Введите пароль');
+
+if (answer) { 'Пароль';
+    console.log('Пароль введен верно')
+} else {
+    console.log('Пароль введен не правильно')
+}
