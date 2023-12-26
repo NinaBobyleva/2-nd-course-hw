f0759245cf54d785441dfd44f4a4c2c0031df51a
Nina Bobyleva <2847171@mail.ru>
2023-12-26 14:48:05 +0300

    Changed the files hw3+1.js and style.css

--- hw3+1.js
+++ hw3+1.js
@@ -1,8 +1,88 @@
-let password = 'Пароль';
-let answer = prompt('Введите пароль');
+// Задание 1
 
-if (answer) { 'Пароль';
-    console.log('Пароль введен верно')
+const password = 'пароль';
+const answer = prompt('Введите пароль');
+
+if (answer === password) {
+    console.log('Пароль введен верно');
 } else {
-    console.log('Пароль введен не правильно')
+    console.log('Пароль введен не правильно');
 }
+
+// Задание 2
+
+let c = Number(prompt('Любое число'))
+
+if (c > 0 && c < 10) {
+    alert('Верно')
+} else {
+    alert('Неверно')
+}
+
+// Задание 3
+
+let d = 9;
+let e = 21;
+
+if (d > 100 || e > 100) {
+    console.log('Верно')
+} else {
+    console.log('Неверно')
+}
+
+// Задание 4
+
+let a = '2';
+let b = '3';
+
+alert(Number(a) + Number(b));
+
+// Задание 5
+
+let monthNumber = 12
+monthNumber = Number(prompt('Введите номер месяца'));
+
+switch (monthNumber) {
+    case 1:
+        alert('Зима')
+        break;
+    case 2:
+        alert('Зима')
+        break;
+    case 3:
+        alert('Весна')
+        break;
+    case 4:
+        alert('Весна')
+        break;
+    case 5:
+        alert('Весна')
+        break;
+    case 6:
+        alert('Лето')
+        break;
+    case 7:
+        alert('Лето')
+        break;
+    case 8:
+        alert('Лето')
+        break;
+    case 9:
+        alert('Осень')
+        break;
+    case 10:
+        alert('Осень')
+        break;
+    case 11:
+        alert('Осень')
+        break;
+    case 12:
+        alert('Зима')
+        break;
+    default:
+        alert('В году 12 месяцев')
+        break;
+}
+
+// Задание 6
+
