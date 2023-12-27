17fc5d23a4dfd41bdcc48f8601bb496385abafd1
Nina Bobyleva <2847171@mail.ru>
2023-12-27 18:12:09 +0300

    Changed the file hw3+1.js

--- hw3+1.js
+++ hw3+1.js
@@ -44,45 +44,59 @@ monthNumber = Number(prompt('Введите номер месяца'));
 
 switch (monthNumber) {
     case 1:
-        alert('Зима')
-        break;
     case 2:
+    case 12:
         alert('Зима')
         break;
     case 3:
-        alert('Весна')
-        break;
     case 4:
-        alert('Весна')
-        break;
     case 5:
         alert('Весна')
         break;
     case 6:
-        alert('Лето')
-        break;
     case 7:
-        alert('Лето')
-        break;
     case 8:
         alert('Лето')
         break;
     case 9:
-        alert('Осень')
-        break;
     case 10:
-        alert('Осень')
-        break;
     case 11:
         alert('Осень')
-        break;
-    case 12:
-        alert('Зима')
         break;
     default:
         alert('В году 12 месяцев')
         break;
 }
 
-// Задание 6
+// Задание 7
 
+let anyNumber = Number(prompt('Пожалуйста, введите любое число.'))
+
+if (anyNumber % 2) {
+    alert('Число нечетное')
+} else {
+    alert('Число четное')
+}
+
+// Задание 8 и 9
+
+let clientOS = Number(prompt('Скачайте приложение. Если у вас iOS введите 0, Android введите 1.'));
+let year = Number(prompt('Какого года выпуска ваш телефон?'))
+let iOS = 0;
+let Android = 1;
+
+if (clientOS === iOS) {
+    if (clientOS === iOS && year >= 2015) {
+        console.log('Установите версию приложения для iOS по ссылке.')
+     } else {
+       console.log('Установите облегченную версию приложения для iOS по ссылке.') 
+    }
+} else if (clientOS === Android) {
+    if (clientOS === Android && year >= 2015) {
+        console.log('Установите версию приложения для Android по ссылке.')
+     } else {
+       console.log('Установите облегченную версию приложения для Android по ссылке.') 
+    }
+} else {
+    console.log('Ввел что-то не то.')
+}
