3c1dd93f22446deb9fd0da210f386ee46ec5ff0d
Nina Bobyleva <2847171@mail.ru>
2024-01-02 22:28:05 +0300

    Changed the file hw5+1.js

---
+++ hw5+1.js
@@ -0,0 +1,60 @@
+// Задание 1
+/*
+let task = (a, b) => {
+   let result = a > b ? console.log(b) : console.log(a);
+   return result
+}
+
+task(3, 8);
+task(5, 5);
+task(9, 6);
+
+// Задание 2
+
+let number = function () {
+    let answer = prompt('Введите число');
+    if (answer % 2 == 0) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+    }
+}
+console.log(number());
+
+// Задание 3.1
+
+function square(a) {
+   console.log(a*a);
+}
+
+square(8);
+
+// Задание 3.2
+
+function square(a) {
+    return a**2; 
+ }
+ 
+ console.log(square(8));
+ 
+
+//  Задание 4
+
+function user() {
+    let answer = prompt('Сколько вам лет?');
+    if (answer >= 0 && answer <= 12) {
+        alert('Привет, друг!')
+    } else if (answer >= 13) {
+        alert('Добро пожаловать!')
+    } else if (answer < 0) {
+        alert('Вы ввели неправильное значение')
+    }
+}
+
+user();
+*/
+// Задание 5
+
+let mult = (a, b) => {
+    
+}
