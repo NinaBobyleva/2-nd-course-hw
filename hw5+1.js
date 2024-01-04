9dc7a7fc2d31e3b2b85ca3bbfa07f0e8b4e07148
Nina Bobyleva <2847171@mail.ru>
2024-01-04 16:59:34 +0300

    Changes

--- hw5+1.js
+++ hw5+1.js
@@ -16,7 +16,7 @@ let number = function () {
     if (answer % 2 == 0) {
         return 'Число четное';
     } else {
-        return 'Число нечетное';
+        return 'Число нечетное забор';
     }
 }
 console.log(number());
